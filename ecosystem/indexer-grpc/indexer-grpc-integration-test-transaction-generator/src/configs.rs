569673cd6b623bbbccfd2a868a4e69d34c7e3cf8
Larry Liu <larry@aptoslabs.com>
2024-05-29 17:41:38 -0700

    update code.

--- ecosystem/indexer-grpc/indexer-grpc-integration-test-transaction-generator/src/configs.rs
+++ ecosystem/indexer-grpc/indexer-grpc-integration-test-transaction-generator/src/configs.rs
@@ -7,14 +7,8 @@ use aptos_protos::transaction::v1::transaction::TransactionType;
 use serde::{Serialize, Deserialize};
 use clap::Parser;
 
-const TRANSACTION_GENERATOR_CONFIG_FILE_NAME: &str = "config.yaml";
 const TEST_CASE_CONFIG_FILE_NAME: &str = "test_case_config.yaml";
 const MOVE_FILE_EXTENSION: &str = "move";
-
-// Design Overview
-// TransactionGeneratorConfig is the overall configuration for the transaction generator.
-// It holds the configuration for the internal node and the path to the test cases main folder.
-// Each test case resides in a folder under the test cases main folder and contains a config file and move files.
 
 /// Args specific to running a node (and its components, e.g. the txn stream) in the
 /// localnet.
@@ -85,17 +79,20 @@ impl TestCase {
     fn load(test_case_folder: PathBuf) -> anyhow::Result<Self> {
         // Makes sure target folder exists.
         if !test_case_folder.is_dir() {
-            return Err(anyhow::anyhow!("Test case folder does not exist or path is not a folder."));
+            return Err(anyhow::anyhow!(format!("Test case folder does not exist or path is not a folder at {:?}.", test_case_folder)));
         }
 
         // Loads the config file.
         let test_case_config_path = test_case_folder.join(TEST_CASE_CONFIG_FILE_NAME);
-        let test_case_config_raw = std::fs::read_to_string(test_case_config_path).context("Config file not found.")?;
-        let test_case_config: TestCaseConfig = serde_yaml::from_str(&test_case_config_raw).context("Config file is malformed.")?;
+        let test_case_config_raw = std::fs::read_to_string(&test_case_config_path)
+            .context(format!("Config file not found at {:?}.", test_case_config_path))?;
+        let test_case_config: TestCaseConfig = serde_yaml::from_str(&test_case_config_raw)
+            .context(format!("Config file is malformed at {:?}.", test_case_config_path))?;
 
         // Scan all move files.
         let mut move_files: Vec<PathBuf> = vec![];
-        let entries =  std::fs::read_dir(&test_case_folder).context("Failed to scan test case folder.")?;
+        let entries =  std::fs::read_dir(&test_case_folder)
+            .context(format!("Failed to scan test case folder at {:?}", test_case_folder))?;
         for entry in entries {
             let entry = entry.context("Failed to scan move files for one test case.")?;
             let path = entry.path();
@@ -116,6 +113,20 @@ impl TestCase {
     }
 }
 
+pub fn load_all_test_cases(test_cases_folder: PathBuf) -> anyhow::Result<Vec<TestCase>> {
+    let mut test_cases = Vec::new();
+    let entries = std::fs::read_dir(&test_cases_folder)
+        .context("Folder does not exist or path is not a folder.")?;
+    for entry in entries {
+        let entry = entry.context("Failed to scan test cases due to FS issue.")?;
+        let path = entry.path();
+        if path.is_dir() {
+            test_cases.push(TestCase::load(path).context("One test case loading failed.")?);
+        }
+    }
+    Ok(test_cases)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -127,18 +138,17 @@ mod tests {
         let dir = tempfile::tempdir().unwrap();
         let test_case_folder = dir.path().to_path_buf();
         let test_case_config_path = test_case_folder.join(TEST_CASE_CONFIG_FILE_NAME);
-        let test_case_config_raw = r#"
-        {
-            number_of_transactions: 10,
-            transaction_type_filter: [TRANSACTION_TYPE_VALIDATOR],
-        }"#;
+        let test_case_config_raw = r#"---
+            number_of_transactions: 10
+            transaction_type_filter:
+                - TRANSACTION_TYPE_VALIDATOR
+        "#;
         std::fs::write(test_case_config_path, test_case_config_raw).unwrap();
         // Create a move file.
         let move_file_path = test_case_folder.join("0.move");
         std::fs::write(move_file_path, "").unwrap();
         let test_case = TestCase::load(test_case_folder);
         assert!(test_case.is_ok());
-
         let test_case = test_case.unwrap();
         assert_eq!(test_case.test_case_config.number_of_transactions, 10);
         assert_eq!(test_case.test_case_config.transaction_type_filter, vec![TransactionType::Validator]);
@@ -151,15 +161,15 @@ mod tests {
         let dir = tempfile::tempdir().unwrap();
         let test_case_folder = dir.path().to_path_buf();
         let test_case_config_path = test_case_folder.join(TEST_CASE_CONFIG_FILE_NAME);
-        let test_case_config_raw = r#"
-        {
-            number_of_transactions: ten,
-            transaction_type_filter: [TRANSACTION_TYPE_VALIDATOR],
-        }"#;
+        let test_case_config_raw = r#"---
+            number_of_transactions: ten
+            transaction_type_filter:
+                - TRANSACTION_TYPE_VALIDATOR
+        "#;
         std::fs::write(test_case_config_path, test_case_config_raw).unwrap();
-        let test_case = TestCase::load(test_case_folder);
+        let test_case= TestCase::load(test_case_folder);
         assert!(test_case.is_err());
-        assert_eq!(test_case.unwrap_err().to_string(), "Config file is malformed.");
+        assert!(test_case.unwrap_err().to_string().contains("Config file is malformed"));
     }
 
     #[test]
@@ -169,7 +179,7 @@ mod tests {
         let test_case_folder = dir.path().to_path_buf();
         let test_case = TestCase::load(test_case_folder);
         assert!(test_case.is_err());
-        assert_eq!(test_case.unwrap_err().to_string(), "Config file not found.");
+        assert!(test_case.unwrap_err().to_string().contains("Config file not found"));
     }
 
     #[test]
@@ -178,7 +188,7 @@ mod tests {
         let file = tempfile::NamedTempFile::new().unwrap();
         let test_case = TestCase::load(file.path().to_path_buf());
         assert!(test_case.is_err());
-        assert_eq!(test_case.unwrap_err().to_string(), "Test case folder does not exist or path is not a folder.");
+        assert!(test_case.unwrap_err().to_string().contains("Test case folder does not exist or path is not a folder"));
     }
 
 
@@ -188,6 +198,60 @@ mod tests {
         let dir = tempfile::tempdir().unwrap();
         let test_cases_folder = dir.path().to_path_buf();
 
+        // Create a test case folder.
+        let test_case_folder = test_cases_folder.join("test_case_1");
+        std::fs::create_dir(&test_case_folder).unwrap();
+        let test_case_config_path = test_case_folder.join(TEST_CASE_CONFIG_FILE_NAME);
+        let test_case_config_raw = r#"---
+            number_of_transactions: 10
+            transaction_type_filter:
+                - TRANSACTION_TYPE_VALIDATOR
+        "#;
+        std::fs::write(test_case_config_path, test_case_config_raw).unwrap();
 
+        // Create a move file.
+        let move_file_path = test_case_folder.join("0.move");
+        std::fs::write(move_file_path, "").unwrap();
+
+        // Verify the test case is loaded successfully.
+        let test_cases = load_all_test_cases(test_cases_folder).unwrap();
+        assert_eq!(test_cases.len(), 1);
+        assert_eq!(test_cases[0].test_case_config.number_of_transactions, 10);
+        assert_eq!(test_cases[0].test_case_config.transaction_type_filter, vec![TransactionType::Validator]);
+        assert_eq!(test_cases[0].move_files.len(), 1);
+    }
+
+    #[test]
+    fn test_test_cases_parsing_with_test_loading_failure() {
+        // tempdir creates a temporary directory and returns a PathBuf to it.
+        let dir = tempfile::tempdir().unwrap();
+        let test_cases_folder = dir.path().to_path_buf();
+
+        // Create a test case folder.
+        let test_case_folder = test_cases_folder.join("test_case_1");
+        std::fs::create_dir(&test_case_folder).unwrap();
+        let test_case_config_path = test_case_folder.join(TEST_CASE_CONFIG_FILE_NAME);
+        let test_case_config_raw = r#"---
+            number_of_transactions: 10
+            transaction_type_filter:
+                - TRANSACTION_TYPE_VALIDATOR
+        "#;
+        std::fs::write(test_case_config_path, test_case_config_raw).unwrap();
+
+        // Malformed config file.
+        let test_case_folder = test_cases_folder.join("test_case_2");
+        std::fs::create_dir(&test_case_folder).unwrap();
+        let test_case_config_path = test_case_folder.join(TEST_CASE_CONFIG_FILE_NAME);
+        let test_case_config_raw = r#"---
+            number_of_transactions: ten
+            transaction_type_filter:
+                - TRANSACTION_TYPE_VALIDATOR
+        "#;
+        std::fs::write(test_case_config_path, test_case_config_raw).unwrap();
+
+        // Verify the test case is loaded successfully.
+        let test_cases = load_all_test_cases(test_cases_folder);
+        assert!(test_cases.is_err());
+        assert!(test_cases.unwrap_err().to_string().contains("One test case loading failed"));
     }
 }
